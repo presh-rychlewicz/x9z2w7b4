54b510705f69ee0d6928bc417dd6fc7402981fb4
Przemyslaw Rychlewicz <przemyslaw.rychlewicz@mettle.co.uk>
2026-07-18 13:05:08 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Welcome to the App</h1>
+      <p>Start building your feature here.</p>
+    </div>
+  )
+}
+
+export default App
