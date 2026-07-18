1cd3d0038d5bc3efbd476d44dfcebc8a76df9fe5
Przemyslaw Rychlewicz <przemyslaw.rychlewicz@mettle.co.uk>
2026-07-18 20:44:41 +0100

    misc

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/x9z2w7b4/' : '/',
+  base: process.env.GITHUB_ACTIONS ? "/x9z2w7b4/" : "/",
   plugins: [react()],
-})
+});
