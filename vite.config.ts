dd5b11873177994cf964ec06b54e04b10778534d
Przemyslaw Rychlewicz <przemyslaw.rychlewicz@mettle.co.uk>
2026-07-18 20:44:27 +0100

    misc

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/x9z2w7b4/' : '/',
   plugins: [react()],
 })
